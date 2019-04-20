da2c1cc464dfee18fa2c09541875d397d6f37cd9
Javier Presmanes Cardama <presmanes3@gmail.com>
2019-04-20 11:04:39 +0100

    Added AbstractSensorClass

---
+++ Proyecto Final Ciclo Superior/src/Main/SensorAbstractClass/SensorClass.h
@@ -0,0 +1,16 @@
+#ifndef _SENSOR_H_
+#define _SENSOR_H_
+
+#include "Arduino.h"
+
+class AbstractSensor {
+public:
+  AbstractSensor();
+  ~AbstractSensor();
+
+  virtual bool read(bool debug = false) = 0;
+  virtual bool setup(bool debug = false) = 0;
+  virtual bool update(bool debug = false) = 0;
+};
+
+#endif // _SENSOR_H_
