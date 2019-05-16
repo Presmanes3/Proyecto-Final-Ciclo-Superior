6dd3e41adca2518a22cc7d6d19d305f149da22f9
Javier Presmanes Cardama <presmanes3@gmail.com>
2019-05-16 21:58:51 +0100

    Solved Bugs

--- Proyecto Final Ciclo Superior/src/Main/ManualDoorControllerClass/ManualDoorController.cpp
+++ Proyecto Final Ciclo Superior/src/Main/ManualDoorControllerClass/ManualDoorController.cpp
@@ -1,21 +1,21 @@
 #include "ManualDoorController.h"
 
-ManualDoorController::ManualDoorController(CapacityManager *capacityManager,
-                                           TimeManager *timeManager,
-                                           Timer *checkTimer,
-                                           Timer *standByTimer)
-    : EventManager(timeManager, checkTimer, standByTimer)
-{
+ManualDoorController::ManualDoorController(CapacityManager* capacityManager,
+                                           TimeManager*     timeManager,
+                                           Timer*           checkTimer,
+                                           Timer*           standByTimer)
+    : EventManager(timeManager, checkTimer, standByTimer) {
   this->capacityManager = capacityManager;
-  this->checkTimer->activateFlag();
-  this->standByTimer->deactivateFlag();
+  this->checkTimer->activateFlag( );
+  this->standByTimer->deactivateFlag( );
 }
 
-void ManualDoorController::setup()
-{
+void ManualDoorController::setup( ) {
 #if IN_OUT_MANUAL_DEBUG
   Serial.println(
       F("===== Iniciando Controlador Apertura Manual Puertas ====="));
+  Serial.println(F(SERIAL_SPLITTER));
+  Serial.println( );
 #endif
   pinMode(MANUAL_DOOR_IN_PIN, INPUT);
   pinMode(MANUAL_DOOR_OUT_PIN, INPUT);
@@ -24,48 +24,42 @@ void ManualDoorController::setup()
   pinMode(MANUAL_OUT_LED, OUTPUT);
 
   this->button_option = false;
-  this->read_flag = false;
+  this->read_flag     = false;
 }
 
-bool ManualDoorController::read()
-{
+bool ManualDoorController::read( ) {
 #if IN_OUT_MANUAL_DEBUG
   Serial.print(F(SERIAL_DEBUG_TAG));
   Serial.println(F("Leyendo Controlador Apertura Manual Puertas"));
 #endif
-  if (digitalRead(MANUAL_DOOR_IN_PIN))
-  {
+  if(digitalRead(MANUAL_DOOR_IN_PIN)) {
 #if IN_OUT_MANUAL_DEBUG
     Serial.print(F(SERIAL_DEBUG_TAG));
     Serial.println(F("Boton Entrada Detectado"));
 #endif
     this->button_option = true;
-    this->read_flag = true;
+    this->read_flag     = true;
     return true;
-  }
-  else if (digitalRead(MANUAL_DOOR_OUT_PIN))
-  {
+  } else if(digitalRead(MANUAL_DOOR_OUT_PIN)) {
 #if IN_OUT_MANUAL_DEBUG
     Serial.print(F(SERIAL_DEBUG_TAG));
     Serial.println(F("Boton Salida Detectado"));
 #endif
     this->button_option = false;
-    this->read_flag = true;
+    this->read_flag     = true;
     return true;
   }
   return false;
 }
 
-void ManualDoorController::turnOnExitLight()
-{
+void ManualDoorController::turnOnExitLight( ) {
 #if IN_OUT_MANUAL_DEBUG
   Serial.print(F(SERIAL_DEBUG_TAG));
   Serial.println(F("Encenciendo Led Salida"));
 #endif
   digitalWrite(MANUAL_OUT_LED, 1);
 }
-void ManualDoorController::turnOnEnterLight()
-{
+void ManualDoorController::turnOnEnterLight( ) {
 #if IN_OUT_MANUAL_DEBUG
   Serial.print(F(SERIAL_DEBUG_TAG));
   Serial.println(F("Encenciendo Led Entrada"));
@@ -73,16 +67,14 @@ void ManualDoorController::turnOnEnterLight()
   digitalWrite(MANUAL_IN_LED, 1);
 }
 
-void ManualDoorController::turnOffExitLight()
-{
+void ManualDoorController::turnOffExitLight( ) {
 #if IN_OUT_MANUAL_DEBUG
   Serial.print(F(SERIAL_DEBUG_TAG));
   Serial.println(F("Apagando Led Entrada"));
 #endif
   digitalWrite(MANUAL_OUT_LED, 0);
 }
-void ManualDoorController::turnOffEnterLight()
-{
+void ManualDoorController::turnOffEnterLight( ) {
 #if IN_OUT_MANUAL_DEBUG
   Serial.print(F(SERIAL_DEBUG_TAG));
   Serial.println(F("Apagando Led Salida"));
@@ -90,8 +82,7 @@ void ManualDoorController::turnOffEnterLight()
   digitalWrite(MANUAL_IN_LED, 0);
 }
 
-bool ManualDoorController::getButtonOption()
-{
+bool ManualDoorController::getButtonOption( ) {
 #if IN_OUT_MANUAL_DEBUG
   Serial.print(F(SERIAL_DEBUG_TAG));
   Serial.println(F("Devolviendo el valor de Button Option"));
@@ -99,51 +90,39 @@ bool ManualDoorController::getButtonOption()
   return this->button_option;
 }
 
-void ManualDoorController::run()
-{
-  if (this->checkTimer->getFlag())
-  {
-    if (this->timeManager->pastMil(*this->checkTimer))
-    {
-      if (this->read())
-      {
+void ManualDoorController::run( ) {
+  if(this->checkTimer->getFlag( )) {
+    if(this->timeManager->pastMil(*this->checkTimer)) {
+      if(this->read( )) {
 
-        if (this->button_option)
-        {
-          this->capacityManager->addPerson();
-          this->capacityManager->showDataSerial();
-          this->turnOnEnterLight();
-        }
-        else
-        {
-          this->capacityManager->removePerson();
-          this->capacityManager->showDataSerial();
-          this->turnOnExitLight();
+        if(this->button_option) {
+          this->capacityManager->addPerson( );
+          this->capacityManager->showDataSerial( );
+          this->turnOnEnterLight( );
+        } else {
+          this->capacityManager->removePerson( );
+          this->capacityManager->showDataSerial( );
+          this->turnOnExitLight( );
         }
 
-        this->checkTimer->deactivateFlag();
-        this->standByTimer->activateFlag();
+        this->checkTimer->deactivateFlag( );
+        this->standByTimer->activateFlag( );
 
-        this->standByTimer->updateReference();
+        this->standByTimer->updateReference( );
       }
     }
   }
-  if (this->standByTimer->getFlag())
-  {
-    if (this->timeManager->pastMil(*this->standByTimer))
-    {
-      if (this->button_option)
-      {
-        this->turnOffEnterLight();
+  if(this->standByTimer->getFlag( )) {
+    if(this->timeManager->pastMil(*this->standByTimer)) {
+      if(this->button_option) {
+        this->turnOffEnterLight( );
+      } else {
+        this->turnOffExitLight( );
       }
-      else
-      {
-        this->turnOffExitLight();
-      }
-      this->standByTimer->deactivateFlag();
-      this->checkTimer->activateFlag();
+      this->standByTimer->deactivateFlag( );
+      this->checkTimer->activateFlag( );
 
-      this->checkTimer->updateReference();
+      this->checkTimer->updateReference( );
     }
   }
 }
